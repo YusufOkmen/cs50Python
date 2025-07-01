6ecb1db1795b6c277b1be7a5740f2ca0e8de5fd2
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-07-01 15:53:57 +0300

    Working on students.csv part2

--- lecture6/csvFile/favourites.py
+++ lecture6/csvFile/favourites.py
@@ -2,21 +2,15 @@ import csv
 
 with open(r"C:\Users\yusuf\OneDrive\Masaüstü\Coding\cs50Python\lecture6\csvFile\students.csv") as file:
     fileReader = csv.DictReader(file)
-
-    # Analys subjects and their favourite count
-    mathCounter, engCounter, peCounter, bioCounter = 0, 0, 0, 0
+    counts = {}
 
     for student in fileReader:
-        if student["Favourite Subject"] == "Math":
-            mathCounter += 1
-        elif student["Favourite Subject"] == "English":
-            engCounter += 1
-        elif student["Favourite Subject"] == "P.E":
-            peCounter += 1
-        elif student["Favourite Subject"] == "Biology":
-            bioCounter += 1
+        favourite = student["Favourite Subject"]
+        if favourite in counts:
+            counts[favourite] += 1
+        else:
+            counts[favourite] = 1
 
-    print(f"Math: {mathCounter}")
-    print(f"English: {engCounter}")
-    print(f"P.E: {peCounter}")
-    print(f"Biology: {bioCounter}")
+# Show favourited subject by highest to lowest.
+for favourite in sorted(counts, key=counts.get, reverse=True):
+    print(f"{favourite}: {counts[favourite]}")
