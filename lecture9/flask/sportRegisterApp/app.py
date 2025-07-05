f7d40e6a2ea4cda22e01e2b83d825b032959e144
hısaeeerq@gmail.com <yusufkmen@hotmail.com>
2025-07-05 18:17:17 +0300

    Making a registration app using Flask part1

---
+++ lecture9/flask/sportRegisterApp/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    if not request.form.get("name") or not request.form.get("sport"):
+        return render_template("failure.html")
+    else:
+        return render_template("success.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug=True) 
